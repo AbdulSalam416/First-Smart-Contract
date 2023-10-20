350eecb10147b55524674b52ae37eb02c63f357f
 AbdulSalam416 <iabdulsalam635@gmail.com>
2023-10-20 14:08:58 +0000

    Initial Commit

---
+++ contracts/Greeting.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+contract Greeting {
+    string ourGreeting;
+
+    function setGreeting (string memory _newGreeting) public   {
+
+        ourGreeting=_newGreeting;
+        
+        
+    }
+}
